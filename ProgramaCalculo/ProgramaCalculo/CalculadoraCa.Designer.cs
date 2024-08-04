4e403add0bc31fc7ddd6537bd349d089b987f947
Ricardo-Meraz <rmeraz08022005@gmail.com>
2024-08-04 15:03:04 -0600

    Add files via upload

---
+++ ProgramaCalculo/ProgramaCalculo/CalculadoraCa.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ProgramaCalculo
+{
+    partial class CalculadoraCa
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFuncion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDerivar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.PictureBoxGrafica = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxGrafica)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFuncion
+            // 
+            this.txtFuncion.Location = new System.Drawing.Point(335, 165);
+            this.txtFuncion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFuncion.Name = "txtFuncion";
+            this.txtFuncion.Size = new System.Drawing.Size(172, 22);
+            this.txtFuncion.TabIndex = 0;
+            this.txtFuncion.Tag = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(477, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CALCULADORA DE DERIVADA (x^n)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 174);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(298, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingresa la Ecuacion A Derivar";
+            // 
+            // btnDerivar
+            // 
+            this.btnDerivar.Location = new System.Drawing.Point(60, 235);
+            this.btnDerivar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDerivar.Name = "btnDerivar";
+            this.btnDerivar.Size = new System.Drawing.Size(129, 28);
+            this.btnDerivar.TabIndex = 3;
+            this.btnDerivar.Text = "DERIVAR";
+            this.btnDerivar.UseVisualStyleBackColor = true;
+            this.btnDerivar.Click += new System.EventHandler(this.btnDerivar_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(55, 352);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Resultado = ";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(172, 347);
+            this.txtResultado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(291, 22);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(197, 235);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(129, 28);
+            this.btnLimpiar.TabIndex = 8;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(335, 235);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(129, 28);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // PictureBoxGrafica
+            // 
+            this.PictureBoxGrafica.Location = new System.Drawing.Point(543, 15);
+            this.PictureBoxGrafica.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.PictureBoxGrafica.Name = "PictureBoxGrafica";
+            this.PictureBoxGrafica.Size = new System.Drawing.Size(631, 549);
+            this.PictureBoxGrafica.TabIndex = 10;
+            this.PictureBoxGrafica.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProgramaCalculo.Properties.Resources.WIN_20240724_11_05_29_Pro__3_;
+            this.pictureBox1.Location = new System.Drawing.Point(129, 425);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 197);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CalculadoraCa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1671, 659);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.PictureBoxGrafica);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDerivar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFuncion);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CalculadoraCa";
+            this.Text = "CALCULADORA CX^n";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxGrafica)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFuncion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDerivar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.PictureBox PictureBoxGrafica;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ProgramaCalculo/ProgramaCalculo/CalculadoraCa.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+
+
+namespace ProgramaCalculo
+{
+    public partial class CalculadoraCa : Form
+    {
+        public CalculadoraCa()
+        {
+            InitializeComponent();
+        }
+        private List<Func<float, float>> funcionesOriginales = new List<Func<float, float>>();
+        private List<Func<float, float>> funcionesDerivadas = new List<Func<float, float>>();
+        private void btnDerivar_Click_1(object sender, EventArgs e)
+{
+    if (string.IsNullOrWhiteSpace(txtFuncion.Text))
+    {
+        txtResultado.Text = "ERROR: Ingresa una función válida";
+        return;
+    }
+
+    string funcion = txtFuncion.Text;
+    Regex regex = new Regex(@"(?<terminos>(?<constantes>-?([a-eA-E]+)?[0-9]*\.?[0-9]*[0-9/]*)[xX]\^(?<exponente>-?([a-eA-E0-9]+)?\.?[0-9]*[0-9/]*))");
+    MatchCollection matches = regex.Matches(funcion);
+
+    List<string> derivadas = new List<string>();
+    funcionesOriginales.Clear();
+    funcionesDerivadas.Clear();
+
+    foreach (Match match in matches)
+    {
+        string Const = match.Groups["constantes"].Value;
+        string Expo = match.Groups["exponente"].Value;
+
+        double coef = ParseCoefficientOrFraction(Const);
+        double expo = ParseCoefficientOrFraction(Expo);
+
+        double resCoef = coef * expo;
+        double resExp = expo - 1;
+
+                string coefOutput = Const.Any(char.IsLetter) ? $"{expo}{Const}" : resCoef.ToString();
+                string expOutput = resExp.ToString();
+
+        string derivada = $"{coefOutput}x^{expOutput}";
+
+        if (expo == 0)
+        {
+            derivada = "0";
+        }
+
+        derivadas.Add(derivada);
+
+        funcionesOriginales.Add(x => (float)(coef * Math.Pow(x, expo)));
+        funcionesDerivadas.Add(x => (float)(resCoef * Math.Pow(x, resExp)));
+    }
+
+    txtResultado.Text = string.Join(" + ", derivadas);
+    DibujarGrafica(funcionesOriginales, funcionesDerivadas);
+}
+
+        private double ParseCoefficientOrFraction(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return 1;
+            }
+
+            input = input.Replace("a", "1").Replace("b", "2").Replace("c", "3").Replace("d", "4").Replace("e", "5");
+            return ParseFraction(input);
+        }
+        private double ParseFraction(string fraction)
+        {
+            if (string.IsNullOrEmpty(fraction))
+                return 1.0;
+
+            string[] parts = fraction.Split('/');
+            if (parts.Length == 2)
+            {
+                double numerator = double.Parse(parts[0]);
+                double denominator = double.Parse(parts[1]);
+                return numerator / denominator;
+            }
+
+            return double.Parse(fraction);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtFuncion.Text = string.Empty;
+            txtResultado.Text = string.Empty;
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void DibujarGrafica(List<Func<float, float>> funcionesOriginales, List<Func<float, float>> funcionesDerivadas)
+        {
+            Bitmap bmp = new Bitmap(PictureBoxGrafica.Width, PictureBoxGrafica.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            int width = PictureBoxGrafica.Width;
+            int height = PictureBoxGrafica.Height;
+
+            g.Clear(Color.White);
+
+            Pen penEjes = new Pen(Color.Black);
+            g.DrawLine(penEjes, 0, height / 2, width, height / 2); // Eje X
+            g.DrawLine(penEjes, width / 2, 0, width / 2, height); // Eje Y
+
+            Font font = new Font("Arial", 8);
+            Brush brush = Brushes.Black;
+
+            float escalaX = 50; // Puntos por unidad en el eje X
+            float escalaY = 50; // Puntos por unidad en el eje Y
+            int unidadesX = width / (int)escalaX / 2; // Número de unidades en el eje X
+            int unidadesY = height / (int)escalaY / 2; // Número de unidades en el eje Y
+
+            for (int i = -unidadesX; i <= unidadesX; i++)
+            {
+                int x = i * (int)escalaX + width / 2;
+                g.DrawString(i.ToString(), font, brush, x, height / 2 + 5);
+                g.DrawLine(penEjes, x, height / 2 - 2, x, height / 2 + 2);
+            }
+
+            for (int i = -unidadesY; i <= unidadesY; i++)
+            {
+                int y = height / 2 - i * (int)escalaY;
+                g.DrawString(i.ToString(), font, brush, width / 2 + 5, y - 5);
+                g.DrawLine(penEjes, width / 2 - 2, y, width / 2 + 2, y);
+            }
+
+            DibujarFunciones(g, funcionesOriginales, Color.Red, width, height, escalaX, escalaY);
+            DibujarFunciones(g, funcionesDerivadas, Color.Blue, width, height, escalaX, escalaY);
+
+            PictureBoxGrafica.Image = bmp;
+        }
+        private void DibujarFunciones(Graphics g, List<Func<float, float>> funciones, Color color, int width, int height, float escalaX, float escalaY)
+        {
+            foreach (var funcion in funciones)
+            {
+                Point? puntoAnterior = null;
+                Pen penFuncion = new Pen(color);
+                for (int x = -width / 2; x < width / 2; x++)
+                {
+                    float realX = x / escalaX;
+                    float realY = funcion(realX);
+
+                    if (float.IsInfinity(realY) || float.IsNaN(realY))
+                    {
+                        puntoAnterior = null;
+                        continue;
+                    }
+
+                    int graficaX = x + width / 2;
+                    int graficaY = height / 2 - (int)(realY * escalaY);
+
+                    if (graficaY < 0 || graficaY >= height)
+                    {
+                        puntoAnterior = null;
+                        continue;
+                    }
+
+                    if (puntoAnterior.HasValue)
+                    {
+                        g.DrawLine(penFuncion, puntoAnterior.Value.X, puntoAnterior.Value.Y, graficaX, graficaY);
+                    }
+
+                    puntoAnterior = new Point(graficaX, graficaY);
+                }
+            }
+        }
+    }
+ 
+}
